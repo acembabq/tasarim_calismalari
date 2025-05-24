cb7a31226dda84b9dff89dfc389177db07f36e41
acembabq <acembabq@gmail.com>
2025-05-24 19:29:44 +0300

    Tasarim ekrani - renk secimi - font ekleme/kullanma

---
+++ android/app/src/main/kotlin/com/example/tasarim_calismalari/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tasarim_calismalari
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/anasayfa.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:tasarim_calismalari/renkler.dart';
+
+class Anasayfa extends StatefulWidget {
+  const Anasayfa({super.key});
+
+  @override
+  State<Anasayfa> createState() => _AnasayfaState();
+}
+
+class _AnasayfaState extends State<Anasayfa> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pizza", style: TextStyle(color: yaziRenk1,fontFamily: "Pacifico", fontSize: 22)),
+        centerTitle: true,
+        backgroundColor: anaRenk,
+      ),
+      body: Center(
+      ),
+    );
+  }
+}
